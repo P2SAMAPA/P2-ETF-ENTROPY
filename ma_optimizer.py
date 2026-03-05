3c9ee04ea6f9093ba50b1a31c797409eefb685dc
P2SAMAPA <pranabpattanaik@gmail.com>
2026-03-05 08:10:35 +0800

    Update ma_optimizer.py

--- ma_optimizer.py
+++ ma_optimizer.py
@@ -1,16 +1,23 @@
 import json
 import os
 import pandas as pd
+
 from transfer_voting import TransferVotingModel
 from backtest import run_backtest
 from strategy_engine import StrategyEngine
 from metrics import calculate_metrics
 
 
-def optimize_ma_window(etf_list, data_dict, price_df, tbill_df,
-                       artifact_path="artifacts"):
+def optimize_ma_window(
+    etf_list,
+    data_dict,
+    price_df,
+    tbill_df,
+    artifact_path="artifacts"
+):
 
     os.makedirs(artifact_path, exist_ok=True)
+
     results = {}
 
     for ma_window in [3, 5]:
@@ -18,41 +25,92 @@ def optimize_ma_window(etf_list, data_dict, price_df, tbill_df,
         features_dict = data_dict[ma_window]["features"]
         targets_dict = data_dict[ma_window]["targets"]
 
-        # Common dates
+        # --------------------------------------------------
+        # 1. Determine Common Date Range Across All ETFs
+        # --------------------------------------------------
+
         common_dates = None
+
         for etf in etf_list:
-            dates = features_dict[etf].index
-            common_dates = dates if common_dates is None else common_dates.intersection(dates)
+            if etf not in features_dict:
+                continue
+
+            etf_dates = features_dict[etf].index
+
+            if common_dates is None:
+                common_dates = etf_dates
+            else:
+                common_dates = common_dates.intersection(etf_dates)
+
+        common_dates = pd.DatetimeIndex(sorted(common_dates))
+
+        # Align price + tbill strictly to common universe
+        price_aligned = price_df.loc[common_dates]
+        tbill_aligned = tbill_df.loc[common_dates]
 
         n = len(common_dates)
+
         train_end = int(n * 0.8)
         val_end = int(n * 0.9)
 
         train_dates = common_dates[:train_end]
         test_dates = common_dates[val_end:]
 
-        X_train = {etf: features_dict[etf].loc[train_dates] for etf in etf_list}
-        y_train = {etf: targets_dict[etf].loc[train_dates] for etf in etf_list}
+        # --------------------------------------------------
+        # 2. Train Model
+        # --------------------------------------------------
+
+        X_train = {}
+        y_train = {}
+
+        for etf in etf_list:
+            X_train[etf] = features_dict[etf].loc[train_dates]
+            y_train[etf] = targets_dict[etf].loc[train_dates]
 
         model = TransferVotingModel(etf_list, ma_window, artifact_path)
-        model.fit(X_train, y_train, price_df.loc[train_dates])
-        model.save(f"{artifact_path}/transfer_voting_MA{ma_window}.pkl")
 
-        # OOS Predictions
+        model.fit(
+            X_train,
+            y_train,
+            price_aligned.loc[train_dates]
+        )
+
+        model_path = f"{artifact_path}/transfer_voting_MA{ma_window}.pkl"
+        model.save(model_path)
+
+        # --------------------------------------------------
+        # 3. Generate OOS Predictions
+        # --------------------------------------------------
+
         predictions_dict = {}
+
         for etf in etf_list:
             X_test = features_dict[etf].loc[test_dates]
+
             preds = model.predict_single_etf(X_test, etf)
-            predictions_dict[etf] = pd.Series(preds, index=test_dates)
+
+            predictions_dict[etf] = pd.Series(
+                preds,
+                index=test_dates
+            )
+
+        # --------------------------------------------------
+        # 4. Backtest
+        # --------------------------------------------------
 
         engine = StrategyEngine(etf_list)
+
         results_bt = run_backtest(
             predictions_dict,
-            price_df,
-            tbill_df,
+            price_aligned,
+            tbill_aligned,
             engine,
             test_dates
         )
+
+        # --------------------------------------------------
+        # 5. Metrics
+        # --------------------------------------------------
 
         metrics = calculate_metrics(
             results_bt["equity_curve"]["strategy"],
@@ -62,6 +120,10 @@ def optimize_ma_window(etf_list, data_dict, price_df, tbill_df,
         )
 
         results[ma_window] = metrics["annualized_return"]
+
+    # ------------------------------------------------------
+    # Select Best MA Window
+    # ------------------------------------------------------
 
     best_ma = max(results, key=results.get)
 
